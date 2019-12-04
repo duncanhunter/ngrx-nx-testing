765b6f6349148c488f2400c78b6feb5346e99391
Duncan Hunter <dh@Duncans-MacBook-Pro.local>
2019-12-04 19:43:53 +1100

    feat(auth) scaffold ngrx

--- apps/demo-ngrx/src/app/app.module.ts
+++ apps/demo-ngrx/src/app/app.module.ts
@@ -1,13 +1,37 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
+
+import { NxModule } from '@nrwl/angular';
+import { StoreModule } from '@ngrx/store';
+import { EffectsModule } from '@ngrx/effects';
+import { StoreDevtoolsModule } from '@ngrx/store-devtools';
+
+import { SharedDataAccessAuthModule } from '@ngrx-workspace/shared/data-access/auth';
+
+import { environment } from '../environments/environment';
+
 
 import { AppComponent } from './app.component';
-import { RouterModule } from '@angular/router';
 
 @NgModule({
   declarations: [AppComponent],
   imports: [
     BrowserModule,
+    NxModule,
+    StoreModule.forRoot(
+      {},
+      {
+        metaReducers: !environment.production ? [] : [],
+        runtimeChecks: {
+          strictActionImmutability: true,
+          strictStateImmutability: true
+        }
+      }
+    ),
+    EffectsModule.forRoot([]),
+    !environment.production ? StoreDevtoolsModule.instrument() : [],
+    SharedDataAccessAuthModule,
     RouterModule.forRoot([], { initialNavigation: 'enabled' })
   ],
   providers: [],
--- libs/shared/data-access/auth/src/lib/shared-data-access-auth.module.ts
+++ libs/shared/data-access/auth/src/lib/shared-data-access-auth.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import * as fromAuth from './+state/auth.reducer';
+import { NxModule } from '@nrwl/angular';
+import { StoreModule } from '@ngrx/store';
 
 @NgModule({
-  imports: [CommonModule]
+  imports: [
+    CommonModule,
+    StoreModule.forFeature(fromAuth.AUTH_FEATURE_KEY, fromAuth.reducer)
+  ]
 })
 export class SharedDataAccessAuthModule {}
